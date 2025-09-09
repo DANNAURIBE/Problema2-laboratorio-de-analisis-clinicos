37b095b67c9a71b6e2379b79eaba07a687fbd26f
DANNAURIBE <danna1666z@gmail.com>
2025-09-08 22:21:25 -0500

    para rama main

--- ejercicio.py
+++ ejercicio.py
@@ -57,7 +57,7 @@ else:
 
 print("Resultado del análisis:", resultado)
 
-"""  danna  """
+"""  danna Uriebe Santiago"""
 
 # -----------------------------------------------------
 # Tabla de referencia de hemoglobina (g/dL)
